5f0fff18293db2f0e07b489c5e5832385501d4aa
Thomas Dubiel <td1729@hotmail.com>
2022-04-20 23:29:59 -0500

    login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do 
   root to: 'main#index'
   get 'about', to: "about#index" 
+
+  delete 'logout', to: "sessions#destroy" 
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
   
   get 'sign_up', to: "registrations#new" 
   post 'sign_up', to: "registrations#create"
