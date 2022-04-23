2a73e06c36618ed6abb9466fa2c328aa0689efea
Thomas Dubiel <td1729@hotmail.com>
2022-04-23 12:57:31 -0500

    finish styling navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+    before_action :current_user 
+    
+    def current_user 
+        if session[:user_id] 
+            Current.user = User.find_by(id: session[:user_id]) 
+        end  
+    end 
 end
